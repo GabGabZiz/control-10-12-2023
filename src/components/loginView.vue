27b8d1ec84624b8b2b75d6c371dba77dc41b2e27
GabGabZiz <gabriela.zizold@ucb.edu.bo>
2023-10-12 20:55:10 -0400

    push

---
+++ src/components/loginView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+/* import Swal from "sweetalert2"; */
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: null,
+    };
+  },
+  mounted() {
+    console.log("mounted() called.......");
+  },
+  methods: {
+    async fetchContries() {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/v1/country"
+        );
+        console.log(response.data);
+
+        //obtener todos los paises
+        this.countries = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+/**Pages */
+import App from "./App.vue";
+import login from "./components/loginView.vue";
+
+/**end pages */
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    name: "login",
+    path: "/login",
+    component: login,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Crea la aplicación y úsala con el router y Pinia
+const app = createApp(App);
+app.use(router);
+
+// Monta la aplicación
+app.mount("#app");
